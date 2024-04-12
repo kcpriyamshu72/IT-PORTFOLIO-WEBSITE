2074766111f2d55ca765cad922c110f5d941f032
Aayush@K.C <144783967+kcpriyamshu72@users.noreply.github.com>
2024-04-12 23:32:01 +0545

    Delete joinus.js

--- joinus.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const form = document.querySelector("#contactForm");
-    form.addEventListener("submit", function(event) {
-        // Form validation logic goes here
-        event.preventDefault(); // Prevent form submission for demonstration
-    });
-});
